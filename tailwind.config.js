811cbe6951aed9a8b5eb388e5b9af9fdddb74f66
wieldhan23 <wieldhan_templar@yahoo.co.id>
2021-12-24 16:59:50 +0700

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-
+const colors = require('tailwindcss/colors');
 module.exports = {
-    content: [
+    purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            colors: {
+                sky: colors.sky,
+                cyan: colors.cyan,
+                lightblue: colors.lightblue,
+                fontFamily: {
+                    sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+                },
             },
         },
-    },
 
-    plugins: [require('@tailwindcss/forms')],
+        variants: {
+            extend: {
+                opacity: ['disabled'],
+            },
+        },
+
+        plugins: [require('@tailwindcss/forms')],
+    },
 };
